5b5e8dc819262cfa6927f077b0c5e70391ae4b42
brettl1991 <brettl1991@gmail.com>
2022-03-22 17:39:33 +0000

    Set the game to the starting point

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+//set the game to the starting point
+
+//selecting elements
+const score0El = document.querySelector('#score--0'); //or other way for id-s
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
